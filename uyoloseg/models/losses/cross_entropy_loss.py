b4fd1f23c9bc134146961686222a50e339eefd90
uyolo1314 <uyolo1314@gmail.com>
2023-01-02 15:23:40 +0000

    first commit

---
+++ uyoloseg/models/losses/cross_entropy_loss.py
@@ -0,0 +1,47 @@
+# ==================================================================
+# Copyright (c) 2023, uyolo.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the
+# distribution.
+# 3. All advertising materials mentioning features or use of this software
+# must display the following acknowledgement:
+# This product includes software developed by the uyolo Group. and
+# its contributors.
+# 4. Neither the name of the Group nor the names of its contributors may
+# be used to endorse or promote products derived from this software
+# without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY UYOLO, GROUP AND CONTRIBUTORS
+# ===================================================================
+import torch
+from torch import nn, Tensor
+from torch.nn import functional as F
+
+class CrossEntropyLoss(nn.Module):
+    def __init__(self, ignore_label: int = 255, weight: Tensor = None, aux_weights: list = [1, 0.4, 0.4]) -> None:
+        super(CrossEntropyLoss, self).__init__()
+        self.aux_weights = aux_weights
+        self.criterion = nn.CrossEntropyLoss(weight=weight, ignore_index=ignore_label)
+
+    def _forward(self, preds: Tensor, labels: Tensor) -> Tensor:
+        # preds in shape [B, C, H, W] and labels in shape [B, H, W]
+        ph, pw = preds.shape[2:4]
+        lh, lw = labels.shape[1:3]
+        if ph != lh or pw != lw:
+            preds = F.interpolate(input=preds, size=(
+                lh, lw), mode='bilinear', align_corners=False)
+        return self.criterion(preds, labels)
+
+    def forward(self, preds: Tensor, labels: Tensor) -> Tensor:
+        if isinstance(preds, tuple):
+            return sum([w * self._forward(pred, labels) for (pred, w) in zip(preds, self.aux_weights)])
+        return self._forward(preds, labels)
