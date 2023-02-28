af0998a4f8215b406f13a7110fa5c211c140e5bc
uyolo1314 <uyolo1314@gmail.com>
2023-02-28 15:14:16 +0000

    update `setup.py`

--- setup.py
+++ setup.py
@@ -2,21 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name='uyoloseg',
-    version='0.0.1',
-    description='SOTA Semantic Segmentation Models',
-    url='https://github.com/uyolo1314/uyolo-segmentation',
+    version='0.0',
+    description='SOTA Real Time Semantic Segmentation Models',
+    url='https://github.com/uyolo-cn/uyolo-segmentation',
     author='uyolo1314',
     author_email='uyolo1314@gmail.com',
     classifiers=[
         "Development Status :: Beta",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     license='Apache License 2.0',
-    packages=find_packages(include=['uyoloseg'], exclude=("config", "tools", "demo")),
+    packages=find_packages(include=['uyoloseg'], exclude=("config", "tools", "demo", "docs")),
     zip_safe=False,
 )
