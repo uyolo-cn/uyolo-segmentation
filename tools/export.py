c85c79a68dfb67da5e17a91613c58caf5de0f925
uyolo <101853326+uyolo1314@users.noreply.github.com>
2023-03-09 16:17:00 +0800

    Update export.py

--- tools/export.py
+++ tools/export.py
@@ -41,6 +41,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--cfg", default='configs/ddrnet.yaml', type=str, help="train config file path")
     parser.add_argument("--model", type=str, help="model file(.pth) path")
+    parser.add_argument("--add-resize", action=store_true, help="add resize op as model final layer")
     parser.add_argument("--include", type=str, default='onnx', nargs='*', help="onnx | openvino | etc.")
     parser.add_argument(
         "--out_path", type=str, default="ddrnet23_slim.onnx", help="Onnx model output path."
@@ -56,6 +57,40 @@ def parse_args():
     update_config(args)
     return args
 
+class SegInterpolate(torch.autograd.Function): 
+ 
+    @staticmethod 
+    def symbolic(g, input, size):
+        return g.op("Resize", 
+                    input, 
+                    g.op("Constant", 
+                         value_t=torch.tensor([], dtype=torch.float32)), 
+                    g.op("Constant", 
+                         value_t=torch.tensor([], dtype=torch.float32)),
+                    size,
+                    coordinate_transformation_mode_s="pytorch_half_pixel",
+                    cubic_coeff_a_f=-0.75,
+                    mode_s='nearest', 
+                    nearest_mode_s="floor") 
+ 
+    @staticmethod 
+    def forward(ctx, input, size): 
+        size = size.tolist() 
+        return F.interpolate(input, size=tuple(size[-2:]), mode='nearest')
+
+class onnxNet(nn.Module):
+    def __init__(self, model):
+        super(onnx_net, self).__init__()
+        self.backone = model
+
+    def forward(self, x, size):
+        x1 = self.backone(x)
+        # y = F.interpolate(x1, size=(self.inter_size[1], self.inter_size[0]), mode='bilinear', align_corners=False)
+        # y = F.softmax(y, dim=1)
+        y = torch.argmax(y, dim=1, keepdim=True).float()
+        y = SegInterpolate.apply(y, size).int()
+        return y
+
 def main():
     args = parse_args()
 
@@ -70,22 +105,28 @@ def main():
     model = build_model(cfg.model)
     pth = torch.load(args.model, map_location=lambda storage, loc: storage)
     model.load_state_dict(pth["state_dict"])
+    if args.add_resize:
+        model = onnxNet(model)
     model.to(device).eval()
 
     dummy_input = torch.autograd.Variable(
         torch.randn(1, 3, args.input_shape[0], args.input_shape[1])
     ).to(device)
+    
+    size_input = torch.tensor([1, 1, 1080, 1920], dtype=torch.int64)
 
     if 'onnx' in args.include:
         torch.onnx.export(
             model,
-            dummy_input,
+            dummy_input if not args.add_resize else (dummy_input, size_input),
             args.out_path,
             verbose=False,
             keep_initializers_as_inputs=True,
             opset_version=11,
-            input_names=["data"],
-            output_names=["output"],
+            input_names=["input_image"] if not args.add_resize else ["input_image", "input_size"],
+            output_names=["preds"],
+            do_constant_folding=True,  # whether to execute constant folding for optimization
+            export_params=True        # store the trained parameter weights inside the model file
         )
         logger.log("Finished exporting onnx ")
 
